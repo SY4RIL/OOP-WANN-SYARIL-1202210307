09422f4adc86e68165c8fcb548ed46b50993d980
SY/\RIL <115794953+SY4RIL@users.noreply.github.com>
2022-12-25 15:49:26 +0700

    Add files via upload

---
+++ TPOOPM3/TP/src/Restaurant.java
@@ -0,0 +1,25 @@
+public class Restaurant(WaitingForPickup boolean, private static final Object lock, foodNumber int){
+    WaitingForPickup = boolean false;
+    new Object();
+    foodNumber = int 1;
+    
+    }
+    public void Run() {
+    @Override
+    public void makeFood()
+        while (true) {
+            try {
+                System.out.println("Enter Customer ID: ");
+                System.out.println("Enter how much food to make: ");
+                Thread.sleep(delay);
+                System,out.println(foodNumber:"Making food number"+foodNumber);
+                System.out.println(WaitingForPickup:"Waiting for the waiter to deliver the food")
+                System.out.println(WaitingForPickup:"Food is ready to deliver")
+                System.out.println(Restaurant:"Tell the restaurant to make another food")
+            }
+        }  try_catch(InterruptedException e) {
+            System.out.println("Something wrong \n");
+        }
+    }
+
+    
